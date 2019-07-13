88191ecd7fea973b772f13d3c0e41602d39f5a21
root <root@ip-172-31-4-211.ap-south-1.compute.internal>
2019-07-13 03:16:18 +0000

    bash-case script

---
+++ shell-scripting/bash-case.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "What is your scripting language ? (0 = EXIT)"
+echo "1) bash"
+echo "2) perl"
+echo "3) python "
+echo "4) None of the above!"
+read case;
+
+case $case in 
+          1) echo "You've selected bash!";;
+          2) echo "You've selected perl";;
+          3) echo "You've selected python";;
+          0) exit 
+
+esac
+
+
