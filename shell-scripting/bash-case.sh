d21ec28c5c892d2312f8a5895074ac4f057080bb
root <root@ip-172-31-4-211.ap-south-1.compute.internal>
2019-07-20 16:45:12 +0000

    added new scripts

--- shell-scripting/bash-case.sh
+++ shell-scripting/bash-case.sh
@@ -11,6 +11,7 @@ case $case in
           1) echo "You've selected bash!";;
           2) echo "You've selected perl";;
           3) echo "You've selected python";;
+          4) echo "None of the above....!!";;
           0) exit 
 
 esac
